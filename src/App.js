60f644f044ba90882e19d052d29e5c247ea5cf14
ssyunyun <1ss.yun025@gmail.com>
2021-01-30 17:16:41 +0900

    [Add] useReducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, {createContext, useState} from 'react';
+import React, {useReducer, createContext, useState} from 'react';
 import './App.css';
+import ComponentA from './components/ComponentA';
+import ComponentB from './components/ComponentB';
+import ComponentC_2 from './components/ComponentC_2';
 import ComponentC from './components/ComponentC';
 import Counter from './components/Counter';
 import CounterHook from './components/CounterHook';
+import CounterReducer from './components/CounterReducer';
 import DataFetch from './components/DataFetch';
 import DataFetchById from './components/DataFetchById';
 import EffectHook from './components/EffectHook';
@@ -13,9 +17,27 @@ import MouseEventEffect from './components/MouseEventEffect';
 export const UserContext = createContext()
 export const LanguageContext = createContext()
 
+export const CountContext = createContext()
+const initialState = {
+  firstCounter: 0,
+}
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'increment1':
+      return {...state, firstCounter: state.firstCounter + action.value}
+    case 'decrement1':
+      return {...state, firstCounter: state.firstCounter - action.value}
+    case 'reset':
+      return initialState
+    default:
+      return state
+  }
+}
+
 function App() {
   const [user, setUser] = useState({name: 'yamada', age:'20'})
   const [language, setLanguage] = useState('日本語')
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
     <div className="App">
       {/* <Counter /> */}
@@ -26,11 +48,21 @@ function App() {
       {/* <MouseEventEffect /> */}
       {/* <DataFetch /> */}
       {/* <DataFetchById /> */}
-      <UserContext.Provider value={user}>
+      {/* <UserContext.Provider value={user}>
         <LanguageContext.Provider value={language}>
           <ComponentC />
         </LanguageContext.Provider>
-      </UserContext.Provider>
+      </UserContext.Provider> */}
+      {/* <CounterReducer /> */}
+
+      <h1>カウント: {count.firstCounter}</h1>
+      <CountContext.Provider
+        value={{ countState: count, countDispatch: dispatch}} >
+        <ComponentA />
+        <ComponentB />
+        <ComponentC_2 />
+      </CountContext.Provider>
+
     </div>
   );
 }
---
+++ src/components/ComponentC_2.js
@@ -0,0 +1,15 @@
+import React, {useContext} from 'react'
+import {CountContext} from '../App'
+
+function ComponentC_2() {
+  const countContext = useContext(CountContext)
+  return (
+    <div>
+      <button onClick={() => countContext.countDispatch({type: 'increment1', value: 1})}>increment1</button>
+      <button onClick={() => countContext.countDispatch({type: 'decrement1', value: 1})}>decrement1</button>
+      <button onClick={() => countContext.countDispatch({type: 'reset'})}>reset</button>
+    </div>
+  )
+}
+
+export default ComponentC_2
